01091a54ae7cb51cda85b0423167468a809fd151
zeyf <zainy0012@gmail.com>
2024-05-10 23:19:49 -0400

    remove order change functions in orderbook

--- orderbook.h
+++ orderbook.h
@@ -3,7 +3,6 @@
 #include <memory>
 #include <unordered_map>
 #include <map>
-#include <exception>
 
 #include "order.h"
 #include "pricing.h"
@@ -19,50 +18,4 @@ private:
     std::unordered_map<OrderId, std::shared_ptr<OrderSurface>> _orders;
     PriceLayer _price_layer;
 public:
-    void createOrder() {};
-
-    void modifyOrder() {};
-
-    void cancelOrder(OrderId id) {
-        if (_orders.empty()) {
-            return;
-        }
-        
-        std::shared_ptr<OrderSurface> order_surface = _orders[id];
-        if (order_surface == nullptr) {
-            return;
-        }
-
-        OrderPointer orderPtr = order_surface->orderPointer;
-        if (orderPtr == nullptr) {
-            return;
-        }
-
-        if (orderPtr->getOrderSide() == Side::BUY) {
-            if (_price_layer.bids.empty()) {
-                return;
-            }
-
-            std::list<OrderPointer> priceLevelBids = _price_layer.bids[orderPtr->getOrderPrice()];
-
-            if (priceLevelBids.empty()) {
-                return;
-            }
-
-            priceLevelBids.erase(order_surface->orderPointerSidePriceIterator);
-            _orders.erase(id);
-        } else if (orderPtr->getOrderSide() == Side::SELL) {
-            if (_price_layer.asks.empty()) {
-                return;
-            }
-            
-            std::list<OrderPointer> priceLevelAsks = _price_layer.asks[orderPtr->getOrderPrice()];
-            if (priceLevelAsks.empty()) {
-                return;
-            }
-
-            priceLevelAsks.erase(order_surface->orderPointerSidePriceIterator);
-            _orders.erase(id);
-        }
-    }
 };
