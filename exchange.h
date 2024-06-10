6cd66f09163219ae1228997d41f3ddf9081fd2de
zeyf <zainy0012@gmail.com>
2024-06-09 17:17:00 -0700

    Created an Exchange class with timezone inclusion, added ExchangeId, added createOrder to exchange

--- exchange.h
+++ exchange.h
@@ -1,12 +1,34 @@
 #include "timing.h"
 #include <unordered_map>
 
-enum class Exchange {
+enum class ExchangeId {
     NYSE,
     NASDAQ
 };
 
-std::unordered_map<Exchange, TimeZone> EXCHANGE_TO_TIMEZONE{
-    {Exchange::NYSE, TimeZone::EST},
-    {Exchange::NASDAQ, TimeZone::EST}
+std::unordered_map<ExchangeId, TimeZone> EXCHANGE_ID_TO_TIMEZONE{
+    {ExchangeId::NYSE, TimeZone::EST},
+    {ExchangeId::NASDAQ, TimeZone::EST}
+};
+
+class Exchange {
+private:
+    ExchangeId _id;
+    TimeZone _timezone;
+    std::unordered_map<TickerSymbol, Orderbook> _orderbooks;
+public:
+    Exchange(ExchangeId id):
+    _id(id),
+    _timezone(EXCHANGE_ID_TO_TIMEZONE.at(id)) {}
+
+    void createOrder(
+        TickerSymbol ticker,
+        OrderType type,
+        Price price,
+        Quantity quantity,
+        Side side,
+        Exchange exchange
+    ) {
+        _orderbooks[ticker].createOrder(ticker, type, price, quantity, side, exchange);
+    };
 };
